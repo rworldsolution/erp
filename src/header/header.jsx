38e9a5399e6ddcce4f32185059a776b85f9b97b9
EDUNEXT <rishav.edunext@gmail.com>
2025-10-29 15:33:33 +0530

    git

---
+++ src/header/header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class header extends Component {
+  render() {
+    return (
+      <div className='header'>header</div>
+    )
+  }
+}
+
+export default header
